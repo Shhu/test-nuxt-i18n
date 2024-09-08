69e673886a391e95a30d19af9efb5ff60193ebfc
yann <lowscaa@gmail.com>
2024-09-08 12:39:40 +0200

    fix: add missing contact translation

--- i18n/locales/en.ts
+++ i18n/locales/en.ts
@@ -1,5 +1,6 @@
 export const en = {
   'welcome': 'welcome',
   'home': 'home',
+  'contact': 'contact',
   'contact us': 'contact us',
 } as const
--- i18n/locales/fr.ts
+++ i18n/locales/fr.ts
@@ -1,5 +1,6 @@
 export const fr = {
   'welcome': 'bienvenue',
   'home': 'accueil',
+  'contact': 'contact',
   'contact us': 'nous contacter',
 } as const
