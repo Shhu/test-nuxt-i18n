d08575b66055e6aa04dbd75aa91df7caa11a7253
yann <lowscaa@gmail.com>
2024-09-08 12:31:36 +0200

    ui: space around

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-5xl mx-auto bg-stone-100 rounded-lg my-8 p-4 font-semibold shadow">
+  <div class="max-w-5xl mx-4 lg:mx-auto bg-stone-100 rounded-lg my-8 p-4 font-semibold shadow">
     <h1>{{ $t('Contact us') }}</h1>
   </div>
 </template>
